e7226de9caa99f32162850b636857ae0c4b6d623
Michal Rynski <michal.rynski@mayeryn.com>
2021-01-14 00:06:46 +0100

    @controller to @restcontroller

--- src/main/java/com/szewczyk/cdtest/HelloWorld.java
+++ src/main/java/com/szewczyk/cdtest/HelloWorld.java
@@ -1,12 +1,8 @@
 package com.szewczyk.cdtest;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @CrossOrigin
 @RequestMapping("/db")
 public class HelloWorld {
